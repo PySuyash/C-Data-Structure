ef2012ab305711481645f915e938b27b7ec75365
PySuyash <unknownup31@gmail.com>
2025-07-27 10:04:48 +0530

    Add Spacing

--- 7_Double_Pointer_to_Function.c
+++ 7_Double_Pointer_to_Function.c
@@ -5,15 +5,16 @@
 void modifyValue(int **ptr) {
     **ptr += 1;
 }
+
 int main() {
 
     int a = 10;
     int *ptr = &a;
 
     printf("Old value of a : %d\n", a);
-    
+
     modifyValue(&ptr);      // Passing the address of pointer to the function.
-    
+
     printf("Modified value of a : %d\n", a);
 
     return 0;
