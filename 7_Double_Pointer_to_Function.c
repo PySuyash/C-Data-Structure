dba9570c50d9120a8e264648a928f6e5c707e80f
PySuyash <unknownup31@gmail.com>
2025-07-27 10:04:02 +0530

    Passing the address of pointer to function

---
+++ 7_Double_Pointer_to_Function.c
@@ -0,0 +1,20 @@
+// Passing double pointers to function.
+
+#include <stdio.h>
+
+void modifyValue(int **ptr) {
+    **ptr += 1;
+}
+int main() {
+
+    int a = 10;
+    int *ptr = &a;
+
+    printf("Old value of a : %d\n", a);
+    
+    modifyValue(&ptr);      // Passing the address of pointer to the function.
+    
+    printf("Modified value of a : %d\n", a);
+
+    return 0;
+}
